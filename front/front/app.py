9868fe0dafaddd6019eb3fe9e833a1a1577a0c8f
Ignacio Agliani <ignacio.agliani@gmail.com>
2025-11-02 10:42:19 -0300

    Cambios en el estilos del footer, de los botones y campos de formularios. Ampliación del contenido de página 'contacto.html' (se añadió un iframe con la dirección de la biblioteca, los horarios de atención, mail y teléfono de contacto). Ampliación del contenido de página 'formulario_enviado.html' (se añadio un parrafo mas).

--- front/front/app.py
+++ front/front/app.py
@@ -25,6 +25,15 @@ def funcionamiento():
 @app.route('/contacto',methods=['GET','POST'])
 def contacto():
     title="¡Contactate con Nosotros!"
+    diccionario_horarios = {
+        "Lunes":"08:30 - 22:00",
+        "Martes":"08:30 - 22:00",
+        "Miercoles":"08:30 - 22:00",
+        "Jueves":"08:30 - 22:00",
+        "Viernes":"08:30 - 22:00",
+        "Sábado":"Cerrado",
+        "Domingo":"Cerrado"
+    }
     if request.method == 'POST':
         nombre = request.form.get('nombre_usuario')
         apellido = request.form.get('apellido_usuario')
@@ -37,7 +46,7 @@ def contacto():
                                 email=mail,
                                 tel=telefono,
                                 mensj=mensaje))
-    return render_template('contacto.html',titulo=title)
+    return render_template('contacto.html',titulo=title,horarios=diccionario_horarios)
 
 @app.route('/formulario_enviado')
 def formulario_enviado():
