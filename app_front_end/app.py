0baf185f133444dc9f7b9a875816db4f45ae17de
Lara Ovejero <lovejero@fi.uba.ar>
2025-11-08 17:16:15 -0300

    mejoras visuales y estructurales en biblioteca.html

--- app_front_end/app.py
+++ app_front_end/app.py
@@ -14,8 +14,65 @@ def sobre_nosotros():
 
 @app.route('/biblioteca')
 def biblioteca():
-    title="Biblioteca"
-    return render_template('biblioteca.html',titulo=title)
+    materias = [
+        {
+            "id": "algebra",
+            "titulo": "Algebra",
+            "libros": [
+                {"titulo": "Algebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png"},
+                {"titulo": "Algebra", "autor": "Hames Stewart", "imagen": "images/algebra2.png"},
+                {"titulo": "Algebraic Art", "autor": "Andrea K. Henderson", "imagen": "images/algebra3.png"},
+                {"titulo": "Fund. del algebra lineal", "autor": "Ron Larson", "imagen": "images/algebra4.png"},
+                {"titulo": "Algebra basica", "autor": "David GOnzales Lopez", "imagen": "images/algebra5.png"},
+                {"titulo": "Algebra lineal elemental", "autor": "Richard Hill", "imagen": "images/algebra6.png"},
+                {"titulo": "Algebra I for dummies", "autor": "Mary Jane Sterling", "imagen": "images/algebra7.png"},
+                {"titulo": "Algebra II for dummies", "autor": "Mary Jane Sterling", "imagen": "images/algebra8.png"},
+            ],
+        },
+        {
+            "id": "analisis",
+            "titulo": "Análisis Matemático",
+            "libros": [
+                {"titulo": "Calculo I", "autor": "James Stewart", "imagen": "images/analisis1.png"},
+                {"titulo": "Analisis Matematico I", "autor": "Tebar FLores", "imagen": "images/analisis2.png"},
+                {"titulo": "Analisis Real y complejo", "autor": "Walter Rudin", "imagen": "images/analisis3.png"},
+                {"titulo": "Calculo Avanzado", "autor": "Tom Apostol", "imagen": "images/analisis4.png"},
+                {"titulo": "Introduccion al Analisis", "autor": "Richard Johnson", "imagen": "images/analisis5.png"},
+                {"titulo": "Geometria analitica", "autor": "LEHMANN", "imagen": "images/analisis6.png"},
+                {"titulo": "Ecuaciones diferenciales", "autor": "Takeuchi, Ramirez, Ruiz", "imagen": "images/analisis7.png"},
+                {"titulo": "909 problemas de calculo integral", "autor": "E. Tebar Flores", "imagen": "images/analisis8.png"},
+            ],
+        },
+        {
+            "id": "ids",
+            "titulo": "Introduccion al desarrollo de software",
+            "libros": [
+                {"titulo": "Bash Guide for Beginners", "autor": "Machtelt Garrels", "imagen": "images/ids1.png"},
+                {"titulo": "Linux programing for dummies", "autor": "Jim Keogh", "imagen": "images/ids2.png"},
+                {"titulo": "Communicating the User Experience", "autor": "Richard Caddick, Steve Cable", "imagen": "images/ids3.png"},
+                {"titulo": "Linux Basics for hackers", "autor": "occupytheweb", "imagen": "images/ids4.png"},
+                {"titulo": "Test Driven Development", "autor": "Kent Beck", "imagen": "images/ids5.png"},
+                {"titulo": "HTML & CSS", "autor": "Thomas A. Powell", "imagen": "images/ids6.png"},
+                {"titulo": "Dominando JavaScript", "autor": "Carlos Azaustre", "imagen": "images/ids7.png"},
+                {"titulo": "Building Web Apps with Python and Flask", "autor": " Malhar Lathkar", "imagen": "images/ids8.png"},
+            ],
+        },
+        {
+            "id": "funda",
+            "titulo": "Fundamentos de programacion",
+            "libros": [
+                {"titulo": "El lenguaje de programacion C", "autor": " Brian W. Kernighan,  Dennis M. Ritchie", "imagen": "images/funda1.png"},
+                {"titulo": "Python a fondo", "autor": "Oscar Ramirez Jimenez", "imagen": "images/funda2.png"},
+                {"titulo": "Lenguaje Ensamblador", "autor": "Casella & Berger", "imagen": "images/funda3.png"},
+                {"titulo": "Java como programar", "autor": "Deitel", "imagen": "images/funda4.png"},
+                {"titulo": "Fundamentos de programacion PHP", "autor": "Ricardo Marcelo", "imagen": "images/funda5.png"},
+                {"titulo": "FUndamentos de programacion XML", "autor": "Dave Mercer", "imagen": "images/funda6.png"},
+                {"titulo": "El gran libro de programacion en C++", "autor": "Alfonso GOnzales Perez", "imagen": "images/funda7.png"},
+                {"titulo": "Aprende python en menos de una semana", "autor": "R.M Lewis", "imagen": "images/funda8.png"},
+            ],
+        },
+    ]
+    return render_template("biblioteca.html", materias=materias)
 
 @app.route('/funcionamiento')
 def funcionamiento():
