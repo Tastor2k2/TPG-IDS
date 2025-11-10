ce2f6621790b6ca66c7d63b4e6037b37bab46ef8
ElMoche <pperezm@fi.uba.ar>
2025-11-10 15:56:16 -0300

    Creado una funcion para el logeo del usuario en el archivo datos_usuarios.py

--- back/src/routes/datos_usuarios.py
+++ back/src/routes/datos_usuarios.py
@@ -7,9 +7,9 @@ datos_usuarios_bp  = Blueprint("datos", __name__)
 @datos_usuarios_bp.route('/registrar', methods=['POST']) #nose como esta puesto en el front el form, sino habria que cambiar registrar y el metodo
 def registrar_usuario():
     data = request.get_json()
-    nombre_usuario = data['nombre']
-    email_usuario = data['email']
-    contraseña_usuario = data['contraseña']
+    nombre_usuario = data.get('nombre')
+    email_usuario = data.get('email')
+    contraseña_usuario = data.get('contraseña')
 
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
@@ -27,7 +27,7 @@ def registrar_usuario():
     if existe:
         cursor.close()
         conn.close()
-        return jsonify({"ERROR: EL USUARIO YA EXISTE"}), 400
+        return jsonify({"error": "EL USUARIO YA EXISTE"}), 400
     
     #si no existe y completo todos los campos:
     cursor.execute("""
@@ -39,4 +39,40 @@ def registrar_usuario():
     cursor.close()
     conn.close()
     
+    @datos_usuarios_bp.route('/login', methods=['POST']) #nose como esta puesto en el front el form, sino habria que cambiar registrar y el metodo
+    def login_usuario():
+        data = request.get_json()
+        email_usuario = data.get('email')
+        contraseña_usuario = data.get('contraseña')
+
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        #chequeamos si el usuario completo todos los campos:
+        if not email_usuario or not contraseña_usuario:
+            return jsonify({"error": "Faltan campos obligatorios"}), 400
+
+
+        #chequeamos si existe el usuario:
+        cursor.execute("SELECT * FROM datos WHERE email_usuario = %s AND contraseña_usuario = %s", (email_usuario, contraseña_usuario,))
+        usuario = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        #si el usuario existe:
+        if usuario:
+                 return jsonify({
+            "mensaje": "Login exitoso",
+            "usuario": {
+                "nombre": usuario["nombre_usuario"],
+                "email": usuario["email_usuario"]
+            }
+        }), 200
+           
+        
+        else:
+             
+        
+            return jsonify({"error": "EMAIL O CONTRASELA INCORRECTOS"}), 401
 
