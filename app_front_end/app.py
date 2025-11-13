3eb29e61ef408aa4a873a822b60320247884fef2
Lara Ovejero <lovejero@fi.uba.ar>
2025-11-13 17:47:03 -0300

    biblioteca de solitud de intercambios de libros (temporal)

--- app_front_end/app.py
+++ app_front_end/app.py
@@ -23,68 +23,6 @@ def sobre_nosotros():
     title="¡Sobre Nosotros!"
     return render_template('sobre_nosotros.html',titulo=title)
 
-@app.route('/biblioteca')
-def biblioteca():
-    materias = [
-        {
-            "id": "algebra",
-            "titulo": "Algebra",
-            "libros": [
-                {"titulo": "Algebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png"},
-                {"titulo": "Algebra", "autor": "Hames Stewart", "imagen": "images/algebra2.png"},
-                {"titulo": "Algebraic Art", "autor": "Andrea K. Henderson", "imagen": "images/algebra3.png"},
-                {"titulo": "Fund. del algebra lineal", "autor": "Ron Larson", "imagen": "images/algebra4.png"},
-                {"titulo": "Algebra basica", "autor": "David GOnzales Lopez", "imagen": "images/algebra5.png"},
-                {"titulo": "Algebra lineal elemental", "autor": "Richard Hill", "imagen": "images/algebra6.png"},
-                {"titulo": "Algebra I for dummies", "autor": "Mary Jane Sterling", "imagen": "images/algebra7.png"},
-                {"titulo": "Algebra II for dummies", "autor": "Mary Jane Sterling", "imagen": "images/algebra8.png"},
-            ],
-        },
-        {
-            "id": "analisis",
-            "titulo": "Análisis Matemático",
-            "libros": [
-                {"titulo": "Calculo I", "autor": "James Stewart", "imagen": "images/analisis1.png"},
-                {"titulo": "Analisis Matematico I", "autor": "Tebar FLores", "imagen": "images/analisis2.png"},
-                {"titulo": "Analisis Real y complejo", "autor": "Walter Rudin", "imagen": "images/analisis3.png"},
-                {"titulo": "Calculo Avanzado", "autor": "Tom Apostol", "imagen": "images/analisis4.png"},
-                {"titulo": "Introduccion al Analisis", "autor": "Richard Johnson", "imagen": "images/analisis5.png"},
-                {"titulo": "Geometria analitica", "autor": "LEHMANN", "imagen": "images/analisis6.png"},
-                {"titulo": "Ecuaciones diferenciales", "autor": "Takeuchi, Ramirez, Ruiz", "imagen": "images/analisis7.png"},
-                {"titulo": "909 problemas de calculo integral", "autor": "E. Tebar Flores", "imagen": "images/analisis8.png"},
-            ],
-        },
-        {
-            "id": "ids",
-            "titulo": "Introduccion al desarrollo de software",
-            "libros": [
-                {"titulo": "Bash Guide for Beginners", "autor": "Machtelt Garrels", "imagen": "images/ids1.png"},
-                {"titulo": "Linux programing for dummies", "autor": "Jim Keogh", "imagen": "images/ids2.png"},
-                {"titulo": "Communicating the User Experience", "autor": "Richard Caddick, Steve Cable", "imagen": "images/ids3.png"},
-                {"titulo": "Linux Basics for hackers", "autor": "occupytheweb", "imagen": "images/ids4.png"},
-                {"titulo": "Test Driven Development", "autor": "Kent Beck", "imagen": "images/ids5.png"},
-                {"titulo": "HTML & CSS", "autor": "Thomas A. Powell", "imagen": "images/ids6.png"},
-                {"titulo": "Dominando JavaScript", "autor": "Carlos Azaustre", "imagen": "images/ids7.png"},
-                {"titulo": "Building Web Apps with Python and Flask", "autor": " Malhar Lathkar", "imagen": "images/ids8.png"},
-            ],
-        },
-        {
-            "id": "funda",
-            "titulo": "Fundamentos de programacion",
-            "libros": [
-                {"titulo": "El lenguaje de programacion C", "autor": " Brian W. Kernighan,  Dennis M. Ritchie", "imagen": "images/funda1.png"},
-                {"titulo": "Python a fondo", "autor": "Oscar Ramirez Jimenez", "imagen": "images/funda2.png"},
-                {"titulo": "Lenguaje Ensamblador", "autor": "Casella & Berger", "imagen": "images/funda3.png"},
-                {"titulo": "Java como programar", "autor": "Deitel", "imagen": "images/funda4.png"},
-                {"titulo": "Fundamentos de programacion PHP", "autor": "Ricardo Marcelo", "imagen": "images/funda5.png"},
-                {"titulo": "FUndamentos de programacion XML", "autor": "Dave Mercer", "imagen": "images/funda6.png"},
-                {"titulo": "El gran libro de programacion en C++", "autor": "Alfonso GOnzales Perez", "imagen": "images/funda7.png"},
-                {"titulo": "Aprende python en menos de una semana", "autor": "R.M Lewis", "imagen": "images/funda8.png"},
-            ],
-        },
-    ]
-    return render_template("biblioteca.html", materias=materias)
-
 @app.route('/funcionamiento')
 def funcionamiento():
     title="¿Cómo Funcionamos?"
@@ -127,54 +65,92 @@ def formulario_enviado():
     data = (nombre,apellido,mail,telefono,mensaje)
     return render_template('formulario_enviado.html',titulo=title,informacion_usuario=data)
 
-@app.route('/solicitud_intercambio', methods = ['GET', 'POST'])
+
+
+
+#traigo los libros del usuario desde la db
+@app.route('/biblioteca')
+def biblioteca():
+    tematicas = [
+        {
+            "id": "algebra",
+            "titulo": "Álgebra",
+            "publicaciones": [
+                {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana López", "facultad": "UNSa"},
+                {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Carlos Díaz", "facultad": "UBA"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana Lópes", "facultad": "UNSa"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana Lópen", "facultad": "UNSa"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana Lópe", "facultad": "UNSa"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana Ló", "facultad": "UNSa"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana L", "facultad": "UNSa"},
+                 {"titulo": "Álgebra de Baldor", "autor": "A. Baldor", "imagen": "images/algebra1.png",
+                 "usuario": "Ana López", "facultad": "UNSa"},
+
+            ],
+        },
+        {
+            "id": "analisis",
+            "titulo": "Análisis Matemático",
+            "publicaciones": [
+                {"titulo": "Cálculo I", "autor": "James Stewart", "imagen": "images/analisis1.png",
+                 "usuario": "Lucía Torres", "facultad": "UTN"}
+            ],
+        },
+        {
+            "id": "ids",
+            "titulo": "Introducción al desarrollo de software",
+            "publicaciones": [
+                {"titulo": "HTML & CSS", "autor": "Thomas A. Powell", "imagen": "images/ids6.png",
+                 "usuario": "Nacho Gómez", "facultad": "UBA"}
+            ],
+        },
+    ]
+    return render_template("biblioteca.html", tematicas=tematicas)
+
+
+
+#mail para solicitar el intercambio (sujeto a cambio con la integracion de la db)
+
+@app.route('/solicitud_intercambio', methods=['GET', 'POST'])
 def formulario_intercambio():
+    #aca buscamos el libro en la db
     if request.method == 'POST':
         nombre_solicitante = request.form.get('nombre-solicitante')
         apellido_solicitante = request.form.get('apellido-solicitante')
         mail_solicitante = request.form.get('mail-solicitante')
         libro_solicitar = request.form.get('libro-solicitar')
         libro_intercambiar = request.form.get('libro-intercambiar')
+        propietario = request.form.get('propietario')
+
         msg = Message(
-            subject = "Nueva solicitud",
-            recipients = [mail_solicitante] 
+            subject="Nueva solicitud de intercambio",
+            recipients=[mail_solicitante, "provisorio@gmail.com"]  #uso los del propietario y solicitante (db)
         )
 
-        msg.html = render_template("mail_intercambio.html", 
-                                   nombre_solicitante = nombre_solicitante, 
-                                   apellido_solicitante = apellido_solicitante, 
-                                   mail_solicitante = mail_solicitante, 
-                                   libro_solicitar = libro_solicitar,
-                                   libro_intercambiar = libro_intercambiar
-                                   )
-        
-        mail.send(msg) 
+        msg.html = render_template("mail_intercambio.html",
+                                   nombre_solicitante=nombre_solicitante,
+                                   apellido_solicitante=apellido_solicitante,
+                                   mail_solicitante=mail_solicitante,
+                                   libro_solicitar=libro_solicitar,
+                                   libro_intercambiar=libro_intercambiar,
+                                   propietario=propietario)
+        mail.send(msg)
         return redirect(url_for('biblioteca'))
+
     else:
-        libro_solicitar = request.args.get('libro', '').replace('%20', ' ')
-        return render_template("formulario_intercambio.html",
-                                materias = [],
-                                libro_seleccionado=libro_solicitar)
-    
-@app.route('/aceptado')
-def aceptado():
-    return render_template('aceptado.html', materias=[])
-
-    
-@app.route("/codigo", methods=['GET', 'POST'])
-def codigo():
-    codigo = "" #se va una vez validado
-    if request.method == 'POST':
-        codigo = request.form.get("codigo")
-        #codigo para verficar en la base de datos
-        return redirect(url_for('aceptado'))
-    return render_template('codigo_intercambio.html', codigo=codigo, materias=[])
-
-
-
-
-
-
+        libro_solicitar = request.args.get('libro', '')
+        propietario = request.args.get('propietario', '')
+        return render_template('formulario_intercambio.html',
+                               tematicas=[],
+                               libro_seleccionado=libro_solicitar,
+                               propietario=propietario)
 
 if __name__ == "__main__":
     app.run("localhost", port="5000",debug=True)
